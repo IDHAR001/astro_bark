b3e03f8a473b2f3ef640fb0aa8ced6d2675b2bb7
IDHAR001 <943967062@qq.com>
2024-01-11 00:29:22 +0800

    update time

---
+++ demo.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import schedule
+import time
+
+# Your Bark token
+lingjie_bark_token = os.environ.get("lingjie_bark_token")
+jacob_bark_token = os.environ.get("jacob_bark_token")
+
+
+# The message to send
+message = "布置给家长要买的辅导书^^"
+
+# The URL of the Bark API
+bark_url = "https://api.day.app/" + lingjie_bark_token + "/"
+
+# Send a message to Bark
+def send_bark_message(message):
+    requests.get(bark_url + message)
+
+send_bark_message(message)
+schedule.every().at("3:30").do(send_bark_message, message)
+
+# Keep the program running
+while True:
+    schedule.run_pending()
+    time.sleep(1)
